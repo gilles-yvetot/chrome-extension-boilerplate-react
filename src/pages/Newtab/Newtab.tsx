36db2de6162f2af801c0d63861fd210a874fc2e7
Gilles Yvetot <g.yvetot@gmail.com>
2023-03-06 20:02:01 -0800

    chore: rm old code

--- src/pages/Newtab/Newtab.tsx
+++ src/pages/Newtab/Newtab.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import App from '../../components/App';
 import './Newtab.css';
 import './Newtab.scss';
-import App from '../../components/App.tsx';
 
 const Newtab = () => {
   return <App />;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Newtab/Newtab.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
-      </header>
-    </div>
-  );
 };
 
 export default Newtab;
